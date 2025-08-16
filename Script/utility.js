6a0737eb407980f3ef04ec3e905ee230af275a64
Motakabber-billah <billah.hstu@gmail.com>
2025-08-16 16:54:55 +0600

    Fair added

---
+++ Script/utility.js
@@ -0,0 +1,89 @@
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-[#1DD100]');
+}
+
+// function removeBackgroundColorById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('bg-[#1DD100]');
+// }
+
+let clickedButtons = [];
+function storeButton(id) {
+    let text = document.getElementById(id).innerText;
+    let index = clickedButtons.indexOf(text);
+    let clickedButtonsLength = clickedButtons.length;
+    
+    if (!clickedButtons.includes(text)) {
+        clickedButtons.push(text);
+        setBackgroundColorById(text);
+        // console.log(clickedButtons);
+
+        let initialSeatNumber = getTextValueById('seat-number');
+        let updatedSeatNumber = initialSeatNumber + 1;
+        console.log(updatedSeatNumber);
+        setTextValueById('seat-number', updatedSeatNumber);
+
+        let initialTotalSeat = getTextValueById('left-seat');
+        // console.log(initialTotalSeat);
+        let updatedLeftSeat = initialTotalSeat - 1;
+        setTextValueById('left-seat', updatedLeftSeat);
+
+        addRow(id);
+
+        let calculatePrice = updatedSeatNumber;
+        console.log(calculatePrice);
+        let totalPrice = calculatePrice * 500;
+        setTextValueById('total-price', totalPrice);
+        
+
+    }
+
+    // if (clickedButtonsLength <= 3) {
+    //     if (!clickedButtons.includes(text)) {
+    //         setBackgroundColorById(text);
+    //         clickedButtons.push(text);
+    //     }
+    // }
+
+}
+
+function addRow(id) {
+    let btn = document.getElementById(id);
+    let seatText = btn.innerText; // get inner text of button
+
+    let container = document.getElementById("ticketContainer");
+
+    // Create new row div
+    let row = document.createElement("div");
+    row.className = "flex items-center justify-between py-2";
+    row.setAttribute("data-seat", seatText);
+
+
+    // Example: random class + fare (you can fetch dynamically)
+    let seatClass = "Economy";
+    let fare = "500";
+
+    row.innerHTML = `
+      <span>${seatText}</span>
+      <span>${seatClass}</span>
+      <span>${fare}</span>
+    `;
+
+    // Append row to container
+    container.appendChild(row);
+}
+
+
+function getTextValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const elementValue = parseInt(elementValueText);
+    return elementValue;
+}
+
+
+function setTextValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
